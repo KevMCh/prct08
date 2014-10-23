a48569fe2ba102b1290d3f7c65fc158d43a97ac2
Nicolas Vargas <alu0100706734@ull.edu.es>
2014-10-23 17:35:47 +0100

    Adiccion en la clase exam

--- lib/exam.rb
+++ lib/exam.rb
@@ -2,13 +2,18 @@ require "exam/version.rb"
 
 
 module Exam
-  # Your code goes here...
+
 
   class Exam
-	def initialize (args)
+		attr_accessor :text,:right, :distractor
+
+	def initialize(args)
 	@text = args[:text]
-	@right = args [:text]
-	@distractions = args [:text]
+	raise ArgumentError, 'Specify :text' unless @text
+	@right = args[:right]
+	raise ArgumentError, 'Specify :right' unless @right
+	@distractor = args[:distractor]
+	raise ArgumentError, 'Specify :distractor' unless @distractor
   end
 
 end

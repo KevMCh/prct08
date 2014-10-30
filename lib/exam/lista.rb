cb9eba104d6a2be39e2b3c009eb9d61c3357cb09
Kevin Martín <alu0100774655@ull.edu.es>
2014-10-30 23:24:27 +0000

    arreglo

--- lib/exam/lista.rb
+++ lib/exam/lista.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Node = Struct.new(:value, :next)
 class Lista
 
@@ -19,30 +18,4 @@ class Lista
         end
 
 end
-=======
-Cell = Struct.new(:value, :next)
 
-
-list = Cell.new(2,nil)
-
-
-def linked_list(value, cell)
-  return Cell.new(value, cell)
-end
-
-
-def recursive_print(list)
-  p list.value
-  recursive_print(list.next) unless list.next == nil
-end
-
-
-
-i = 0
-10.times {
-  i += 1
-  list = linked_list(i, list)
-}
-
-recursive_print(list) 
->>>>>>> 6d517e42483e5babd3ef86a6311d0f90cc9d27b7

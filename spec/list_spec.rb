cecbf65d771e9808a74c08342c721f987967cdcc
Kevin Martín <alu0100774655@ull.edu.es>
2014-10-31 00:15:48 +0000

    Expectativa una lista (dos nodos)

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -6,13 +6,21 @@ describe Exam do
 	before :each do 
 
 		@nodo = Node.new(2,nil)
+
+		@nodo1 = Node.new(4,nil)
 	end
 
-	describe "Creacion nodo" do
+	describe "Creacion clase lista" do
 
 		it "Comprobacion de valor" do
 
 			@nodo.value.should eq(2)
+
+		end
+
+		it "Añadir un segundo nodo a la lista" do
+
+			@nodo.next.should eq(@nodo1)
 
 		end
 

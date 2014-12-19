85a7525cfed4989f590af12964687de68df8b938
Kevin Martín Chinea <alu0100774665@ull.edu.es>
2014-12-19 08:51:51 +0000

    Expectativa hilo crecer

--- spec/orange_spec.rb
+++ spec/orange_spec.rb
@@ -5,51 +5,62 @@ describe 'Naranjero' do
 
     before :each do
     
-        @Naranjo = Orange.new
-    
+        @NaranjoP = Orange.new
+        
+        @NaranjoH = Orange.new
+        
     end
     
     it 'Atributos de la clase.' do
         
-        expect(@Naranjo.edad).to eq(0)
-        expect(@Naranjo.altura).to eq(0)
-        expect(@Naranjo.numnaranjas).to eq(0) 
+        expect(@NaranjoP.edad).to eq(0)
+        expect(@NaranjoP.altura).to eq(0)
+        expect(@NaranjoP.numnaranjas).to eq(0) 
         
     end
     
     it 'Método uno_más' do
         
-        @Naranjo.uno_mas
-        expect(@Naranjo.edad).to eq(1)
-        expect(@Naranjo.altura).to eq(0.75)
+        @NaranjoP.uno_mas
+        expect(@NaranjoP.edad).to eq(1)
+        expect(@NaranjoP.altura).to eq(0.75)
         
     end
     
     it 'Método recolectar_una' do
         
-        @Naranjo.uno_mas
-        @Naranjo.uno_mas
-        expect(@Naranjo.edad).to eq(2)
-        expect(@Naranjo.altura).to eq(1.5)
-        expect(@Naranjo.numnaranjas).to eq(0)
-        expect(@Naranjo.recolectar_una).to eq("No hay naranjas")
-        @Naranjo.uno_mas
-        @Naranjo.uno_mas
-        @Naranjo.uno_mas
-        @Naranjo.uno_mas
-        @Naranjo.uno_mas
-        expect(@Naranjo.numnaranjas).to eq(9)
-        expect(@Naranjo.recolectar_una).to eq("Que buena que estaba")
-        @Naranjo.uno_mas
-        @Naranjo.uno_mas
-        @Naranjo.uno_mas
-        @Naranjo.uno_mas
-        expect(@Naranjo.edad).to eq(11)
-        expect(@Naranjo.recolectar_una).to eq("El naranjero se ha secado")
-        @Naranjo.uno_mas
-        expect(@Naranjo.numnaranjas).to eq(0)
+        @NaranjoP.uno_mas
+        @NaranjoP.uno_mas
+        expect(@NaranjoP.edad).to eq(2)
+        expect(@NaranjoP.altura).to eq(1.5)
+        expect(@NaranjoP.numnaranjas).to eq(0)
+        expect(@NaranjoP.recolectar_una).to eq("No hay naranjas")
+        @NaranjoP.uno_mas
+        @NaranjoP.uno_mas
+        @NaranjoP.uno_mas
+        @NaranjoP.uno_mas
+        @NaranjoP.uno_mas
+        expect(@NaranjoP.numnaranjas).to eq(9)
+        expect(@NaranjoP.recolectar_una).to eq("Que buena que estaba")
+        @NaranjoP.uno_mas
+        @NaranjoP.uno_mas
+        @NaranjoP.uno_mas
+        @NaranjoP.uno_mas
+        expect(@NaranjoP.edad).to eq(11)
+        expect(@NaranjoP.recolectar_una).to eq("El naranjero se ha secado")
+        @NaranjoP.uno_mas
+        expect(@NaranjoP.numnaranjas).to eq(0)
         
         
     end
-     
+    
+    it 'Hilo crecimiento' do
+        
+        @crecer.join
+    
+        expect(@NaranjoH.edad).to eq(4)
+        expect(@NaranjoH.altura).to eq(3.0)
+        expect(@NaranjoH.numnaranjas).to eq(0) 
+        
+    end
 end
